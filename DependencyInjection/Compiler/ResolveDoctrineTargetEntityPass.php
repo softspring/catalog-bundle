a16e7580e1bdfac56ee63d25a66c7f36e4bf2fb3
Javi H. Gil <javihgil@gmail.com>
2019-10-21 12:41:06 +0200

    Use SfsCoreBundle doctrine target entity resolver abstract compiler pass

--- DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
+++ DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
@@ -4,42 +4,25 @@ namespace Softspring\CatalogBundle\DependencyInjection\Compiler;
 
 use Softspring\CatalogBundle\Model\ProductInterface;
 use Softspring\CatalogBundle\Model\ModelInterface;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Softspring\CoreBundle\DependencyInjection\Compiler\AbstractResolveDoctrineTargetEntityPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
 
-class ResolveDoctrineTargetEntityPass implements CompilerPassInterface
+class ResolveDoctrineTargetEntityPass extends AbstractResolveDoctrineTargetEntityPass
 {
+    /**
+     * @inheritDoc
+     */
+    protected function getEntityManagerName(ContainerBuilder $container): string
+    {
+        return $container->getParameter('sfs_catalog.entity_manager_name');
+    }
+
     /**
      * @param ContainerBuilder $container
      */
     public function process(ContainerBuilder $container)
     {
-        // configure catalog entity
-        $productClass = $container->getParameter('sfs_catalog.product.class');
-        if (!class_implements($productClass, ProductInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $productClass, ProductInterface::class));
-        }
-        $this->setTargetEntity($container, ProductInterface::class, $productClass);
-
-        // configure model entity
-        if ($modelClass = $container->getParameter('sfs_catalog.model.class')) {
-            if (!class_implements($modelClass, ModelInterface::class)) {
-                throw new LogicException(sprintf('%s class must implements %s interface', $modelClass, ModelInterface::class));
-            }
-
-            $this->setTargetEntity($container, ModelInterface::class, $modelClass);
-        }
-    }
-
-    private function setTargetEntity(ContainerBuilder $container, string $interface, string $class)
-    {
-        $resolveTargetEntityListener = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
-
-        if (!$resolveTargetEntityListener->hasTag('doctrine.event_subscriber')) {
-            $resolveTargetEntityListener->addTag('doctrine.event_subscriber');
-        }
-
-        $resolveTargetEntityListener->addMethodCall('addResolveTargetEntity', [$interface, $class, [$container->getParameter('sfs_catalog.entity_manager_name')]]);
+        $this->setTargetEntityFromParameter('sfs_catalog.product.class', ProductInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_catalog.model.class', ModelInterface::class, $container, false);
     }
 }
